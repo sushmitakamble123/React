def521b0787944f644bc8725e0eed057207b259c
Sushmita Kamble <sushmitakamble501@gmail.com>
2026-05-05 16:18:10 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import StudentCrud from './component/StudentCrud';
+
+function App() {
+  return (
+    <div>
+      <h1>My React App</h1>
+
+      
+      <StudentCrud />
+      
+    </div>
+  );
+}
+
+export default App;
